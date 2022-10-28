8d94371d066e95d3912d9ab6b3a9471aeeac013c
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2022-10-28 09:49:00 -0500

    Modifying the main for preloading and adding elements to the manifest

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import com.example.myapplication.ViewModel.Preload
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Log.i("MainActivity","")
+        val intent = Intent(this, Preload::class.java)
+        startActivity(intent)
+        finish()
     }
 }
