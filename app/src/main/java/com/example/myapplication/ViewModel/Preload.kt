cdb1342f3330476813cfcb8791297bbc13aab259
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2022-10-28 09:48:04 -0500

    New preload view

--- app/src/main/java/com/example/myapplication/ViewModel/Preload.kt
+++ app/src/main/java/com/example/myapplication/ViewModel/Preload.kt
@@ -1,12 +1,25 @@
 package com.example.myapplication.ViewModel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import com.example.myapplication.R
+import com.example.myapplication.ViewModel.Home.Home
 
 class Preload : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preload)
+
+        Handler().postDelayed(Runnable {
+            kotlin.run {
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
+        },1000)
+
+        Log.i("MainActivity","Lanzando layout")
     }
 }
