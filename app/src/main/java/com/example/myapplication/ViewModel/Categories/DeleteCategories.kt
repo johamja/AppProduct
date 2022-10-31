72fdb936b4e4104d2d77ce90b29707509a670036
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2022-10-31 11:01:36 -0500

    Adding files for task categories

---
+++ app/src/main/java/com/example/myapplication/ViewModel/Categories/DeleteCategories.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ViewModel.Categories
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class DeleteCategories : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete_categories)
+    }
+}
