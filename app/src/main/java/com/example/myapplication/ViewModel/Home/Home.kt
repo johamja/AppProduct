a4cb09baf793c65f73666c3adf80823b7aa399b6
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2022-11-01 22:51:33 -0500

    previous modification to see the operation of the category layout

--- app/src/main/java/com/example/myapplication/ViewModel/Home/Home.kt
+++ app/src/main/java/com/example/myapplication/ViewModel/Home/Home.kt
@@ -1,9 +1,14 @@
 package com.example.myapplication.ViewModel.Home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.myapplication.Features.LOG
 import com.example.myapplication.R
+import com.example.myapplication.ViewModel.Categories.VMCategories
+import com.example.myapplication.ViewModel.Login.UserData
 
 class Home : AppCompatActivity() {
 
@@ -13,5 +18,10 @@ class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         LOG.Info(NameClass,"Inicio de la actividad")
+
+        findViewById<Button>(R.id.ActivityHome_categorias).setOnClickListener{
+            val intent = Intent(this, VMCategories::class.java);
+            startActivity(intent)
+        }
     }
 }
