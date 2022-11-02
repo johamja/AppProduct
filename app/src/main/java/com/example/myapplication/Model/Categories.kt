7308eb2312706747f71f9b115c221e572878a8e7
Joham Jaider Morales Franco <johamfranco318@gmail.com>
2022-11-01 22:44:18 -0500

    Creating the class data category

---
+++ app/src/main/java/com/example/myapplication/Model/Categories.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.Model
+
+import android.graphics.Color
+
+
+data class Categories(val name: String, val description:String,val theme: Color, val task: Task, val id: Int) {
+
+
+}
